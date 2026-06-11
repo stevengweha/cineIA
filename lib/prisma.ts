8ee79108491de6ee161d0438e37504f2f307a09b
stevengweha <ngwehasteve@gmail.com>
2026-06-11 03:35:22 +0200

    v1 stable : je crois 😎  encore

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,17 @@
-/// PR
+import { PrismaClient } from '@prisma/client';
 
-import { PrismaClient } from '@prisma/client'
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// PrismaClient est attaché à l'objet "global" en développement pour éviter
-// d'épuiser la limite de connexions à la base de données.
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    // Cette option désactive les prepared statements au niveau du driver
+    // Cela résout le conflit "already exists" sur Vercel/Serverless
+    datasources: {
+      db: {
+        url: `${process.env.DATABASE_URL}?pgbouncer=true`,
+      },
+    },
+  });
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
--- middleware.ts
+++ middleware.ts
@@ -25,8 +25,6 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/', request.url));
   }
 
-  
-
   return NextResponse.next();
 }
 
